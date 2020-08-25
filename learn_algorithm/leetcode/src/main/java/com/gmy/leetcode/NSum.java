ad5e2726fdb0c223013b65f530de585feffd65c4
guanmengyuan <guanmengyuan01@baidu.com>
2020-08-25 21:20:12 +0800

    add Nsum

--- learn_algorithm/leetcode/src/main/java/com/gmy/leetcode/NSum.java
+++ learn_algorithm/leetcode/src/main/java/com/gmy/leetcode/NSum.java
@@ -38,28 +38,6 @@ public class NSum {
         }
     }
 
-    private static void getResult(List<List<Integer>> result, List<Integer> cur, int candidates[], int target, int start) {
-        if (target == 0) {
-            result.add(new ArrayList<>(cur));
-            return;
-        }
-        for (int i = start; i < candidates.length; i++) {
-            if (target < candidates[i])
-                continue;
-            //选择当前节点，类似于从当前节点开始往下遍历
-            cur.add(candidates[i]);
-            getResult(result, cur, candidates, target - candidates[i], i);
-            //回到当前节点的时候我们把当前节点给移除,
-            // 然后通过循环走同一层的其他节点。
-            //举个例子，比如上面图中，最开始的时候
-            // 我们先选择2，然后沿着这个分支走下去，
-            //当回到当前分支的时候我们把2给移除，然后
-            // 选择同一层的下一个3，沿着这个节点
-            //分支走下去……
-            cur.remove(cur.size() - 1);
-        }
-    }
-
     public static void main(String[] args) {
         int[] is = {2, 3, 5};
         List<List<Integer>> lists = combinationSum(is, 8);
