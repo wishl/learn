5b45a9536eafdd10a3058856a9a8a4b9ad498e21
guanmengyuan <guanmengyuan01@baidu.com>
2021-02-04 16:26:52 +0800

    添加算法

--- learn_algorithm/leetcode/src/main/java/com/gmy/leetcode/link/ListNode.java
+++ learn_algorithm/leetcode/src/main/java/com/gmy/leetcode/link/ListNode.java
@@ -1,14 +1,30 @@
 package com.gmy.leetcode.link;
 
 public class ListNode {
+
     public int val;
     public ListNode next;
+
     ListNode() {}
+
     ListNode(int val) { this.val = val; }
+    
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    public void setNext(int val) {
+    
+    public ListNode setNext(int val) {
         ListNode listNode = new ListNode(val);
         this.next = listNode;
+        return this.next;
     }
 
+    @Override
+    public String toString() {
+        String result =  "{" +
+                "\"val\":" + val;
+        if (next != null) {
+            result += ", \"next\":" + next.toString();
+        }
+        result += "}";
+        return result;
+    }
 }
--- learn_algorithm/leetcode/src/main/java/com/gmy/leetcode/link/MidLink.java
+++ learn_algorithm/leetcode/src/main/java/com/gmy/leetcode/link/MidLink.java
@@ -1,4 +1,26 @@
 package com.gmy.leetcode.link;
 
+/**
+ * 寻找链表中点
+ */
 public class MidLink {
+
+    public static ListNode findMid(ListNode head) {
+        ListNode l1 = head;
+        ListNode l2 = head;
+        while (l2 != null && l2.next != null) {
+            l1 = head.next;
+            l2 = head.next.next;
+        }
+        return l1;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.setNext(2).setNext(3).setNext(4);
+        System.out.println(listNode);
+        ListNode revert = findMid(listNode);
+        System.out.println(revert);
+    }
+
 }
